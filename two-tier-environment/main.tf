aecdd8a62743babab0b47079bbe3423d9be8fc44
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2019-02-26 11:10:19 -0800

    Cleaning up the unused ec2 module

--- two-tier-environment/main.tf
+++ two-tier-environment/main.tf
@@ -5,17 +5,6 @@ module "vpc_networking" {
   private_cidrs = "${var.private_cidrs}"
 }
 
-/*module "ec2_instance" {
-  source = "./ec2_instance"
-  count = "${var.instance_count}"
-  ami = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  key_name = "${var.key_name}"
-  vpc_security_group_ids = "[${module.vpc_networking.security_group}]"
-  subnet_id  = "${module.vpc_networking.public_subnets}"
-  user_data = "${var.user_data}"
-}*/
-
 module "ec2_instance" {
   source         = "./ec2_instance"
   instance_count = "${var.instance_count}"
