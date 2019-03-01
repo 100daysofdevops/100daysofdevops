7f949dc14c7a801bfda1711c7627b099fb619a0a
Prashant Lakhera <devops.everyday.challenge@gmail.com>
2019-03-01 09:31:23 -0800

    Checking SNS code

---
+++ two-tier-environment/sns/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_sns_topic" "alarm" {
+  name = "alarms-topic"
+
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
+
+  provisioner "local-exec" {
+    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.alarm_actions}"
+  }
+}
---
+++ two-tier-environment/sns/variables.tf
@@ -0,0 +1,1 @@
+variable "alarm_actions" {}
