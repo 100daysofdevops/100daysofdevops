dd43e388a37ac6ba6949079faea02717571d3719
Prashant Lakhera <devops.everyday.challenge@gmail.com>
2019-03-05 08:01:16 -0800

    Encrypting EBS using KMS

---
+++ kms_ebs/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+resource "aws_ebs_volume" "my-test-kms-ebs" {
+  availability_zone = "us-west-2a"
+  size              = 10
+  type              = "gp2"
+  encrypted = true
+  kms_key_id        = "${var.kms_key}"
+}
