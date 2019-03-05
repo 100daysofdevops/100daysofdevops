eed61de4369de59bc752fa5715627cd35f55e8ed
Prashant Lakhera <devops.everyday.challenge@gmail.com>
2019-03-05 08:02:04 -0800

    fixing formatting error

--- kms_ebs/main.tf
+++ kms_ebs/main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   region = "us-west-2"
 }
+
 resource "aws_ebs_volume" "my-test-kms-ebs" {
   availability_zone = "us-west-2a"
   size              = 10
   type              = "gp2"
-  encrypted = true
+  encrypted         = true
   kms_key_id        = "${var.kms_key}"
 }
