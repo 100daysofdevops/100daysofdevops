15d2a3175cc03176176f0f10dc2bd2517c75f475
Prashant Lakhera <devops.everyday.challenge@gmail.com>
2019-03-16 13:59:24 -0700

    Fixing formatting issue

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -2,8 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-
 resource "aws_instance" "example" {
-  ami                    = "ami-01ed306a12b7d1c96)"
-  instance_type          = "t2.micro"
+  ami           = "ami-01ed306a12b7d1c96)"
+  instance_type = "t2.micro"
 }
