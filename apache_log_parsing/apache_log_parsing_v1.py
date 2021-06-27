f1198140eea198b7c5d9f38f777e845e44241594
100daysofdevops <100daysofdevops@gmail.com>
2021-06-27 11:04:27 -0700

    adding apache log parsing code

---
+++ apache_log_parsing/apache_log_parsing_v1.py
@@ -0,0 +1,20 @@
+import re
+from collections import Counter
+import csv
+import argparse
+
+my_parser = argparse.ArgumentParser(description='Reading the log file')
+my_parser.add_argument("logfile",
+                       help='Please enter the logfile to parse',type=argparse.FileType('r'))
+args = my_parser.parse_args()
+
+
+logreg="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+with args.logfile as f:
+    fread = f.read()
+    ip_list = re.findall(logreg, fread)
+    with open("ipnewcount.csv", "w") as f:
+        fwritercsv = csv.writer(f)
+        fwritercsv.writerow(["IP_Address", "Count"])
+        for k, v in Counter(ip_list).items():
+            fwritercsv.writerow([k, v])
---
+++ apache_log_parsing/apache_log_parsing_v2.py
@@ -0,0 +1,38 @@
+import re
+from collections import Counter
+import csv
+import argparse
+
+
+logreg="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+
+# Create the parser
+my_parser = argparse.ArgumentParser(description='Reading the log and csv file')
+my_parser.add_argument("logfile",
+                       help='Please enter the logfile to parse',type=argparse.FileType('r'))
+args = my_parser.parse_args()
+
+
+def read_file(logfile):
+    with args.logfile as f:
+        log = f.read()
+        ip_list = re.findall(logreg,log)
+        return ip_list
+
+def read_count():
+    ip_list = read_file(args.logfile)
+    ip_count = Counter(ip_list)
+    return ip_count.items()
+
+
+def csv_write():
+    counter = read_count()
+    with open("ip_count.csv",'w') as f:
+        fwriter = csv.writer(f)
+
+        fwriter.writerow(["IP_Address","Count"])
+        for item,val in counter:
+            fwriter.writerow([item, val])
+
+if __name__ == '__main__':
+    csv_write()
