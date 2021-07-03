11e8634a6d4e64599da32a29049be599cfecc1e1
Manzoor Ahamed <ahamedm@meredith.com>
2021-07-03 12:34:57 +0530

    Added try/except and implemented the logic into the function

--- 02-file-search.py
+++ 02-file-search.py
@@ -4,6 +4,21 @@
 import pathlib
 import argparse
 
+## Function
+def file_search(dir,file):
+  try:
+    if dir.is_dir():
+      #print("Directory exists")
+      if file.is_file():
+        print("The file %s exist in the %s directory" %(args.arg2,args.arg1))
+      else:
+        print("The dir %s exist but the file %s is not in the directory" %(args.arg1,args.arg2))
+    else:
+      print("The directory %s doesnt exist" %args.arg1)
+  except:
+    print("Something went wrong")
+
+
 ## Create the parser
 parser = argparse.ArgumentParser()
 
@@ -14,16 +29,7 @@ parser.add_argument("arg2", help="File Name to search for in the dir")
 ## Execute the parse_arg() method
 args = parser.parse_args()
 
-
-
 dir = pathlib.Path(args.arg1)
 file = pathlib.Path(args.arg1,args.arg2)
 
-if dir.is_dir():
-  print("Directory exists")
-  if file.is_file():
-    print("The file %s exist in the %s directory" %(args.arg2,args.arg1))
-  else:
-    print("The dir %s exist but the file %s is not in the directory" %(args.arg1,args.arg2))
-else:
-  print("The directory %s doesnt exist" %args.arg1)
+file_search(dir,file)
