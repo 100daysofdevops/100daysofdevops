0867abd72b398a013594754b84e39bb8253a90ef
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-07-11 18:12:52 -0700

    Adding parameters to specify key

--- paramiko/paramiko_test.py
+++ paramiko/paramiko_test.py
@@ -7,6 +7,8 @@ password=os.environ.get('PASSWORD')
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname='44.X.X.X',username='centos',password=password) # export the password export PASSWORD='<your password>' in the shell
+# If you want to connect using key
+# ssh.connect(hostname='<ip of remote server>',username='centos',key_filename='<location of key>')
 stdin,stdout,stderr = ssh.exec_command('free -m')
 time.sleep(5)
 print(stdout.readlines())
