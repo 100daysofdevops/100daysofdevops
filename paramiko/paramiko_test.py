f178919fc06b2c3154f3f44d3e5fdc453f68ea33
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-07-11 18:07:28 -0700

    Update paramiko_test.py

--- paramiko/paramiko_test.py
+++ paramiko/paramiko_test.py
@@ -1,8 +1,12 @@
 import paramiko
 import time
+import os
+
+password=os.environ.get('PASSWORD')
+
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname='<ip address of remote host>',username='centos',password='<password>')
+ssh.connect(hostname='44.X.X.X',username='centos',password=password) # export the password export PASSWORD='<your password>' in the shell
 stdin,stdout,stderr = ssh.exec_command('free -m')
 time.sleep(5)
 print(stdout.readlines())
