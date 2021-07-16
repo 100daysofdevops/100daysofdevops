6e53a6afb81abfe6a2775a3e850cbf6b046808b8
100daysofdevops <100daysofdevops@gmail.com>
2021-07-15 17:41:20 -0700

    updated terraform code with access and secret key

--- jenkins-terraform-pipeline/ec2_pipeline/variables.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/variables.tf
@@ -1,7 +1,8 @@
 variable "AWS_ACCESS_KEY_ID" {
-  type = string
+  type    = string
   default = "AKIA47TQRQ43FLD5M335"
+}
 variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
+  type    = string
   default = "nC3gdeMpYWclsUEx7HoS2zlTrvemA4gNId9TJ+pj"
 }
