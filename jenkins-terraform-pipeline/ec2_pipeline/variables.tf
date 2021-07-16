44236bd1337e1614bc8338d12c44d1b755fedb0d
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-07-15 17:45:36 -0700

    Update variables.tf

--- jenkins-terraform-pipeline/ec2_pipeline/variables.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/variables.tf
@@ -1,8 +1,6 @@
 variable "AWS_ACCESS_KEY_ID" {
   type    = string
-  default = "AKIA47TQRQ43FLD5M335"
 }
 variable "AWS_SECRET_ACCESS_KEY" {
   type    = string
-  default = "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"
 }
