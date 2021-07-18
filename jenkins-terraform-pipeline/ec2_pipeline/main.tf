e8514fd65d4af84aa49d0309ecb2888f26ead20e
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-07-18 10:34:37 -0700

    Update main.tf

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -1,10 +1,8 @@
 provider "aws" {
   region = "us-west-2"
-  #shared_credentials_file = "/var/jenkins_home/.aws/credentials"
-
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0721c9af7b9b75114"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 }
