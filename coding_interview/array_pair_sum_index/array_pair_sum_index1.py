f1967c17787248dc517d352be6a837109b8ecdb8
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-08-13 09:43:43 -0700

    Create array_pair_sum_index1.py

---
+++ coding_interview/array_pair_sum_index/array_pair_sum_index1.py
@@ -0,0 +1,7 @@
+#Using Brute Force Approach
+
+def twosum(nums,target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[j] == target - nums[i]:
+                return [i,j]
