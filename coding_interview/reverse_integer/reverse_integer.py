51d5b2025780fbc6fa59d25d66e24129126e7915
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-08-13 22:18:57 -0700

    Create reverse_integer.py

---
+++ coding_interview/reverse_integer/reverse_integer.py
@@ -0,0 +1,28 @@
+# Leet Code Problem: https://leetcode.com/problems/reverse-integer/
+# Modulus and division of negative number shows some unexpected result int(divider/number) and number % -divisor
+
+def divide(number, divider):
+    return int(number/divider)
+
+def mod(number,divisor):
+    if number < 0:
+        return number % -divisor
+    return number % divisor
+
+MAX_INT = 2 ** 31 -1
+MIN_INT = -2 ** 31
+
+def reverse(x):
+    res = 0
+    while x:
+        pop = mod(x,10)
+        x = divide(x,10)
+        if res > divide(MAX_INT, 10) or (res == divide(MAX_INT,10) and pop > 7):
+            return 0
+        if res < divide(MIN_INT, 10) or (res == divide(MIN_INT,10) and pop < -8):
+            return 0
+        res = res * 10 + pop
+    return res
+
+
+print(reverse(123))
