b7b8796418b28942f859aa4fe786b18b48e41795
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-08-13 21:47:13 -0700

    Update palindrome_check.py

--- coding_interview/palindrome_check/palindrome_check.py
+++ coding_interview/palindrome_check/palindrome_check.py
@@ -1,6 +1,7 @@
 # Leet Code Problem: https://leetcode.com/problems/palindrome-number/submissions/
 # Negative number can't be palindrome -123 reverse is 321-
 # Some other way to solve the problem is to convert integer to string and then reverse it
+# Time complexity O(n) and Space Complexity 0(1)
 def ispalindrome(x):
     if x < 0:
         return True
