92bb85b59ae823220aba6cbc9ef8358ad0f7d66c
100daysofdevops <47483190+100daysofdevops@users.noreply.github.com>
2021-08-14 09:11:22 -0700

    Create length_of_longest_substring.py

---
+++ coding_interview/length_of_longest_substring/length_of_longest_substring.py
@@ -0,0 +1,14 @@
+def lengthOfLongestSubstring(s):
+    charSet = set()
+    l = 0
+    res = 0
+
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        res = max(res, r - l + 1)
+    return res
+
+print(lengthOfLongestSubstring("abcabcbb"))
